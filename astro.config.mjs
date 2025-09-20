3204b937c97301ffc7bd3a464556dae723c2b6a4
Jon <jonurnieta@gmail.com>
2025-09-20 12:04:59 +0200

    Configure server settings and site URL in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	server: { port: 1234, host: true },
+	site: 'https://example.com'
+});
