ddc119d57acf8fb2047b6d1445b1632aeb57ee84
Jon <jonurnieta@gmail.com>
2025-09-23 09:19:29 +0200

    wip

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,5 @@
 import { z, defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
-import type { Langs } from './i18n/ui';
 
 const news = defineCollection({
 	loader: glob({ pattern: '**/*.md', base: './src/data/news' }),
