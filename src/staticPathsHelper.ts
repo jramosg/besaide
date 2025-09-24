cc54294c20263d38eec2c3d1ffa7eda5be8d74e8
Jon <jonurnieta@gmail.com>
2025-09-24 08:18:35 +0200

    refactor: streamline news post sorting and filtering logic

--- src/staticPathsHelper.ts
+++ src/staticPathsHelper.ts
@@ -7,41 +7,37 @@ type Options = {
 	pageSize?: number;
 };
 
-export async function NewsStaticPaths(
-	lang: Langs,
-	paginate: PaginateFunction,
-	opts: Options = { pageSize: 10 }
-): Promise<ReturnType<PaginateFunction>> {
+const sortedAndFilteredNewsPosts = async (lang: Langs) => {
 	// load collection
-	const allNewsPosts = await getCollection('news');
-	const sortedAllNewsPosts = allNewsPosts.sort((a, b) => {
+	let allNewsPosts = await getCollection('news');
+	allNewsPosts.sort((a, b) => {
 		const dateA = new Date(a.data.date);
 		const dateB = new Date(b.data.date);
 		return dateB.getTime() - dateA.getTime(); // Newest first
 	});
 
 	// filter by language if entries provide language metadata
-	const filtered = sortedAllNewsPosts.filter(entry => {
+	return allNewsPosts.filter(entry => {
 		const entryLang = entry?.data?.lang ?? null;
 		if (!entryLang) return true;
 		return entryLang === lang;
 	});
+};
 
-	return paginate(filtered, { pageSize: opts.pageSize });
+export async function NewsStaticPaths(
+	lang: Langs,
+	paginate: PaginateFunction,
+	opts: Options = { pageSize: 10 }
+): Promise<ReturnType<PaginateFunction>> {
+	return paginate(await sortedAndFilteredNewsPosts(lang), {
+		pageSize: opts.pageSize
+	});
 }
 
 export async function SingleNewsStaticPaths(lang: Langs) {
-	// load collection
-	const allNewsPosts = await getCollection('news');
+	const posts = await sortedAndFilteredNewsPosts(lang);
 
-	// filter by language if entries provide language metadata
-	const filtered = allNewsPosts.filter(entry => {
-		const entryLang = entry?.data?.lang ?? null;
-		if (!entryLang) return true;
-		return entryLang === lang;
-	});
-
-	return filtered.map(post => {
+	return posts.map(post => {
 		return {
 			params: {
 				singleNewsId: slugify(post.id)
