3091347c034c91864b389d355e3177f69a580e95
jramosg <jonurnieta@gmail.com>
2025-11-12 22:11:00 +0100

    feat: refactor ExtrasTable component to improve pricing structure and enhance accessibility

--- src/schemas/federation.ts
+++ src/schemas/federation.ts
@@ -14,31 +14,29 @@ export const federationCollectionSchema = z.object({
 	),
 	infoCardEu: z.string(),
 	infoCardEs: z.string(),
-	extras: z
-		.array(
-			z.object({
-				nameEu: z.string(),
-				nameEs: z.string(),
-				descriptionEu: z.string().optional(),
-				descriptionEs: z.string().optional(),
-				pricing: z.discriminatedUnion('discriminant', [
-					z.object({
-						discriminant: z.literal('fourColumn'),
-						value: z.object({
-							priceChildren: z.number().optional(),
-							priceYouth: z.number().optional(),
-							priceAdults: z.number().optional(),
-							priceSeniors: z.number().optional()
-						})
-					}),
-					z.object({
-						discriminant: z.literal('twoColumn'),
-						value: z.object({
-							priceAll: z.number().optional()
-						})
+	extras: z.array(
+		z.object({
+			nameEu: z.string(),
+			nameEs: z.string(),
+			descriptionEu: z.string().optional(),
+			descriptionEs: z.string().optional(),
+			pricing: z.discriminatedUnion('discriminant', [
+				z.object({
+					discriminant: z.literal('fourColumn'),
+					value: z.object({
+						priceChildren: z.number().optional(),
+						priceYouth: z.number().optional(),
+						priceAdults: z.number().optional(),
+						priceSeniors: z.number().optional()
 					})
-				])
-			})
-		)
-		.optional()
+				}),
+				z.object({
+					discriminant: z.literal('twoColumn'),
+					value: z.object({
+						priceAll: z.number().optional()
+					})
+				})
+			])
+		})
+	)
 });
