437de110487570a86aebe6fed3e03b4b328dd5a6
jramosg <jonurnieta@gmail.com>
2025-11-17 20:19:10 +0100

    fix: make membership description in European language optional

--- src/schemas/homepage.ts
+++ src/schemas/homepage.ts
@@ -3,6 +3,6 @@ import { z } from 'astro:content';
 export const homepageSchema = z.object({
 	membershipTitleEu: z.string(),
 	membershipTitleEs: z.string(),
-	membershipDescriptionEu: z.string(),
+	membershipDescriptionEu: z.string().optional(),
 	membershipDescriptionEs: z.string().optional()
 });
